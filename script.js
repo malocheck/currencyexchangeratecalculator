67245730d67f946bcad152ffc936ba1ad7cd3089
malocheck <maros.pecho@gmail.com>
2022-05-03 16:29:49 +0200

    Update

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-const fromCurrencyNames = new XMLHttpRequest();
-fromCurrencyNames.open('GET', 'https://openexchangerates.org/api/currencies.json?app_id=2c6d838f744844cd81ef87ec5ba83624');
-fromCurrencyNames.onload = function() {
-    const listOfCurencies = JSON.parse(fromCurrencyNames.response);
-    const listOfCurShortcut = Object.keys(listOfCurencies);
+const exchangeRate = new XMLHttpRequest();
+exchangeRate.open('GET', 'https://openexchangerates.org/api/latest.json?app_id=2c6d838f744844cd81ef87ec5ba83624')
+exchangeRate.onload = function(){
+    const listOfExchangeRates = JSON.parse(exchangeRate.response);
+    const exchangeRatesCurrencies = Object.keys(listOfExchangeRates.rates);
 
-    for (let i = 0; i < listOfCurShortcut.length; i++){
-        let currencyOption = document.createElement('option');
-        currencyOption.innerHTML = listOfCurShortcut[i];
-        let visibleListOfCurrencies = document.getElementById('fromcurrencytypeone');
-        visibleListOfCurrencies.append(currencyOption);
+    for (let i = 0; i < exchangeRatesCurrencies.length; i++){
+        //FROM CURRENCY INPUT
+        let fromCurrencyOption = document.createElement('option');
+        fromCurrencyOption.innerHTML = exchangeRatesCurrencies[i];
+        if (fromCurrencyOption.innerHTML === "USD"){
+            fromCurrencyOption.setAttribute("selected", "true");
+        };
+        let visibleListOfFromCurrencies = document.getElementById('fromcurrencytypeone');
+        visibleListOfFromCurrencies.append(fromCurrencyOption);
+
+        //TO CURRENCY INPUT
+        let toCurrencyOption = document.createElement('option');
+        toCurrencyOption.innerHTML = exchangeRatesCurrencies[i];
+        if (toCurrencyOption.innerHTML === "EUR"){
+            toCurrencyOption.setAttribute("selected", "true");
+        };
+        let visibleListOfToCurrencies = document.getElementById('fromcurrencytypetwo');
+        visibleListOfToCurrencies.append(toCurrencyOption);
     };
-}
-
-const toCurrencyNames = new XMLHttpRequest();
-toCurrencyNames.open('GET', 'https://openexchangerates.org/api/currencies.json?app_id=2c6d838f744844cd81ef87ec5ba83624');
-toCurrencyNames.onload = function() {
-    const listOfCurencies = JSON.parse(toCurrencyNames.response);
-    const listOfCurShortcut = Object.keys(listOfCurencies);
-
-    for (let i = 0; i < listOfCurShortcut.length; i++){
-        let currencyOption = document.createElement('option');
-        currencyOption.innerHTML = listOfCurShortcut[i];
-        let visibleListOfCurrencies = document.getElementById('fromcurrencytypetwo');
-        visibleListOfCurrencies.append(currencyOption);
-    };
-}
 
 
-fromCurrencyNames.send(); 
-toCurrencyNames.send();
+    console.log(exchangeRatesCurrencies);
+    console.log(listOfExchangeRates.rates);
+    
+
+    }
+
+exchangeRate.send();
